92b6d6b99f4d3985fd1940aa06d81585eaa87eb8
Ellsy23 <dlwnsgus3153@naver.com>
2023-11-20 21:05:14 +0900

    feat: 플러그인 및 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  define: {
+    _global: {},
+  },
+  server: {
+    https: true,
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://galaxy4276.asuscomm.com:8081/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
