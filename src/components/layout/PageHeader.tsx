2ce5491bdcae0da0bcbb8b932f85687a837a90f7
Ellsy23 <dlwnsgus3153@naver.com>
2023-11-20 21:40:00 +0900

    feat: public-header

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -1,7 +1,14 @@
-import { FC, PropsWithChildren } from 'react';
-import { Box, Image } from '@chakra-ui/react';
+import { FC, PropsWithChildren, useState } from 'react';
+import { Box, Image, Flex, Spacer, Link, HStack, Text, Center, Input } from '@chakra-ui/react';
+import { Search, PersonCircle, BoxArrowInRight, Cart } from 'react-bootstrap-icons';
 
 const PageHeader: FC<PropsWithChildren> = () => {
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+
+  const toggleSearchBar = () => {
+    setIsSearchBarVisible(!isSearchBarVisible);
+  };
+
   return (
     <Box
       as="header"
@@ -14,7 +21,80 @@ const PageHeader: FC<PropsWithChildren> = () => {
       backgroundColor="white"
       zIndex={999}
       alignContent="center"
-    ></Box>
+    >
+      <Flex justify="space-between" align="center" h="100%" alignItems="center" pl={60}>
+        <Image src="/src/images/name_logo.png" alt="Logo" h="60px" />
+
+        <HStack spacing={10} ml={8} align="center">
+          <Link href="/all">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>전체</Center>
+            </Text>
+          </Link>
+          <Link href="/exhibition">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>전시회</Center>
+            </Text>
+          </Link>
+          <Link href="/concert">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>콘서트</Center>
+            </Text>
+          </Link>
+          <Link href="/musical">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>뮤지컬</Center>
+            </Text>
+          </Link>
+          <Link href="/play">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>연극</Center>
+            </Text>
+          </Link>
+        </HStack>
+
+        <Spacer />
+
+        <HStack spacing={10} mr={8} align="center" pr={60}>
+          <Text fontWeight="bold" letterSpacing="0.1em" onClick={toggleSearchBar} cursor="pointer">
+            <Center>
+              <Search /> SEARCH
+            </Center>
+          </Text>
+          <Link href="/login">
+            <Text fontWeight="bold" letterSpacing="0.1em" _hover={{ textDecoration: 'underline' }}>
+              <Center>
+                <BoxArrowInRight /> LOGIN
+              </Center>
+            </Text>
+          </Link>
+          <Link href="/mypage">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>
+                <PersonCircle /> MYPAGE
+              </Center>
+            </Text>
+          </Link>
+          <Link href="/cart">
+            <Text fontWeight="bold" letterSpacing="0.1em">
+              <Center>
+                <Cart /> CART
+              </Center>
+            </Text>
+          </Link>
+        </HStack>
+      </Flex>
+      {isSearchBarVisible && (
+        <Box p={4} bg="white" position="absolute" top="100%" left={0} right={0} boxShadow="sm">
+          <Input
+            placeholder="검색어를 입력해주세요."
+            size="sm"
+            borderColor="gray.400"
+            _hover={{ borderColor: 'gray.600' }}
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
