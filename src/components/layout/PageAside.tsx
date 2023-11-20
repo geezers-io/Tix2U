4cde313be929d59864e42a4d30930a554ad69174
Ellsy23 <dlwnsgus3153@naver.com>
2023-11-20 22:48:12 +0900

    feat: footer 작성

--- src/components/layout/PageAside.tsx
+++ src/components/layout/PageAside.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 
 const PageAside: FC = () => {
   return (
@@ -11,10 +11,18 @@ const PageAside: FC = () => {
       top={0}
       paddingTop={{ base: 0, md: '8px' }}
       boxShadow="0 2px 2px -2px rgba(0,0,0,.3)"
-      backgroundColor="white"
+      backgroundColor="lightgray"
       zIndex={999}
     >
-      Aside
+      <Flex align="center" justify="space-between" h="100%">
+        <Image src="/src/images/logo.png" alt="Logo" w="50px" h="50px" />
+        <Box borderLeft="1px solid #ccc" height="60%" mx="4" />
+        <Flex direction="column" justify="center" align="flex-end">
+          <Text fontSize="sm">개인정보처리방침</Text>
+          <Text fontSize="sm">이용약관</Text>
+          <Text fontSize="sm">이용안내</Text>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
