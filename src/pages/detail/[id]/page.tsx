b468a456cb7ffb5b543a631ea2df016164055ee3
heejung <acb4287@naver.com>
2023-11-26 01:24:30 +0900

    feat: 2차 퍼블리싱

--- src/pages/detail/[id]/page.tsx
+++ src/pages/detail/[id]/page.tsx
@@ -6,7 +6,6 @@ import {
   Image,
   Text,
   Heading,
-  Divider,
   Tabs,
   TabList,
   Tab,
@@ -14,6 +13,13 @@ import {
   TabPanel,
   Textarea,
   Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
 } from '@chakra-ui/react';
 import ImageViewer from '@/components/ImageViewer';
 import TicketingButton from '@/components/TicketingButton';
@@ -41,20 +47,20 @@ const DetailPage: FC = () => {
   }, []);
   return (
     <Box p="10px 200px" bg="purple.50">
-      <Box bgColor="white" p={5} minHeight="1000px">
+      <Box bgColor="white" p={5} fontSize="lg">
         <Flex p="50px">
           <Flex flex={1} w="300px" h="100%" m="auto 0">
             <Image className="MainPoster" src="/public/poster/concert.jpg" />
           </Flex>
 
           <Flex flexDirection="column" m="50px" w="100%" flex={2}>
-            <Flex gap={3}>
+            <Flex gap={3} marginBottom="20px">
               <Badge size="lg" fontSize="lg" p="5px">
                 콘서트
               </Badge>
               <Heading>김범수 콘서트</Heading>
             </Flex>
-            <Divider p={5} />
+
             <Flex flexDirection="column" gap={10}>
               <Flex>
                 <Flex w="100px">
@@ -93,12 +99,12 @@ const DetailPage: FC = () => {
           </Flex>
         </Flex>
 
-        <Box h="1000px">
+        <Box minH="1000px">
           <Tabs isFitted variant="enclosed">
             <TabList mb="1em">
               <Tab>상세 정보</Tab>
-              <Tab>관람후기</Tab>
-              <Tab>장소정보</Tab>
+              <Tab>관람 후기</Tab>
+              <Tab>장소 정보</Tab>
               <Tab>예매/취소 안내</Tab>
             </TabList>
             <TabPanels>
@@ -110,7 +116,7 @@ const DetailPage: FC = () => {
               <TabPanel>
                 <Box>
                   <Flex gap={3}>
-                    <Heading size="lg">관람후기</Heading>
+                    <Heading size="lg">관람 후기</Heading>
                     <Heading size="lg" color="red">
                       0
                     </Heading>
@@ -122,6 +128,102 @@ const DetailPage: FC = () => {
                       등록
                     </Button>
                   </Flex>
+                </Box>
+              </TabPanel>
+              <TabPanel>장소</TabPanel>
+              <TabPanel>
+                <Box>
+                  <Heading size="xl">예매/취소 안내</Heading>
+                  <Box>
+                    <Heading size="lg">티켓 수령 안내</Heading>
+
+                    <Text as="b">
+                      <br />
+                      1. 일반배송
+                    </Text>
+                    <Text>
+                      예매 완료(결제 완료)확인 후, 인편배송으로 영업일 기준 10일 이내 티켓을 수령하실 수 있습니다.
+                      <br />
+                      티켓을 배송하기 위한 배송료는 고객이 부담합니다. <br />
+                      행사 또는 관람일에 따라 일반배송 선택이 제한될 수 있습니다. <br />
+                      <br />​
+                    </Text>
+                    <Text as="b">2. 현장수령</Text>
+                    <Text>
+                      행사 당일 공연 시작 시간 1시간 전 ~ 30분 전까지 행사장 매표소에서 티켓을 수령하실 수 있습니다.
+                      <br />
+                      현장 매표소에서 예매 완료 SMS 또는 예매번호 및 예매자 정보 확인 후 티켓을 수령할 수 있습니다.
+                      <br />
+                      기획사 정책 또는 행사일에 따라 현장 수령 방법의 선택이 제한될 수 있습니다.​ <br />
+                      <br /> <br /> <br />​
+                    </Text>
+
+                    <Heading>예매 취소 안내</Heading>
+                    <Text>
+                      예매 당일 취소하는 경우 이외에는 예매수수료는 환불되지 않습니다. <br />
+                      <br />
+                      티켓 예매 후 7일 이내 취소 시 취소수수료는 부과되지 않습니다. <br />
+                      단, 예매 후 7일 이내라도 취소시점이 관람일로부터 10일 이내라면 그에 해당하는 취소수수료가
+                      부과됩니다. <br />
+                      <br />
+                      배송 받은 티켓의 반품 접수는 취소가능시간 이내(영업일 기준)에 우편(빠른 등기) 또는 본사 반품을
+                      통해서 입고 완료 건에 한하여 취소 가능하며, 입고 일을 기준으로 취소수수료 적용됩니다. <br />
+                      일반우편 또는 택배로 반송 시 발생되는 분실, 지연 도착 등의 문제는 티켓링크에서 책임지지 않으니
+                      이점 유의하시기 바랍니다.
+                      <br />
+                      <br />
+                    </Text>
+                    <TableContainer>
+                      <Table variant="simple">
+                        <Thead>
+                          <Tr>
+                            <Th>관람일 구분</Th>
+                            <Th>취소 가능 시간</Th>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          <Tr>
+                            <Td>일반</Td>
+                            <Td>관람일 1일전 17:00시까지</Td>
+                          </Tr>
+                          <Tr>
+                            <Td>​관람일이 연휴기간이거나 연휴 다음날인 경우​</Td>
+                            <Td>
+                              연휴 시작일 1일전 17:00시까지
+                              <br />
+                              (연휴기간이 토요일~화요일인 경우 금요일 17시까지 취소가능)
+                            </Td>
+                          </Tr>
+                        </Tbody>
+                      </Table>
+                    </TableContainer>
+                    <Text>
+                      일부 공연의 경우 취소 가능 시간이 상이할 수 있습니다.​​​ <br />
+                      <br />
+                    </Text>
+                    <Heading>티켓 환불 안내</Heading>
+                    <Text>
+                      예매취소 시 취소수수료와 배송료를 제외한 나머지 금액이 환불 됩니다.
+                      <br />
+                      <br />
+                      취소수수료는 상품별로 상이할 수 있으며 상품 상세정보 하단에서 확인할 수 있습니다. <br />
+                      <br />
+                      무통장입금으로 결제한 경우 환불처리를 위해 예매자 본인명의의 계좌정보(예금주, 은행, 계좌번호)를
+                      입력해야 하며 접수일로부터 3~5일(영업일기준)이내 환불 받을 수 있습니다. <br />
+                      상품에 따라 환불 시 송금수수료 500원이 부과될 수 있습니다. ​<br />
+                      <br />
+                      신용카드로 결제한 경우 취소일로부터 3~6일(영업일기준)이내 카드사에서 승인취소를 확인할 수
+                      있습니다. <br />
+                      <br />
+                      계좌이체, 실시간 계좌출금으로 결제한 경우 취소 시 1~2일(영업일 기준)이내 환불 됩니다.
+                      <br />
+                      휴대폰결제로 결제한 경우 당월 취소 시 즉시 환불되나 익월 취소 시에는 최대 60일까지 소요됩니다.
+                      <br />
+                      PAYCO포인트, 예매권, 상품권으로 결제한 경우 취소 시 즉시 계정으로 환불 됩니다.
+                      <br />
+                      <br />
+                    </Text>
+                  </Box>
                 </Box>
               </TabPanel>
             </TabPanels>
