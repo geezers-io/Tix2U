a6582a3a9ed290fd5cbb7d1ff1dc2ac4c2a4e25f
heejung <acb4287@naver.com>
2023-11-28 22:45:11 +0900

    Revert "feat: 아이디 찾기 페이지 퍼블리싱"
    This reverts commit c43b10345d2164a1663f23e68d41aeb6ba6637fe.
    
    # Conflicts:
    #	src/App.tsx
    #	src/pages/login/page.tsx

---
+++ src/pages/login/page.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Box,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Divider,
+  Flex,
+  AbsoluteCenter,
+  Text,
+} from '@chakra-ui/react';
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const handleClick = () => setShowPassword(!showPassword);
+  return (
+    <Box p="10px 5%" bg="purple.50">
+      <Box bgColor="white" p={5} minHeight="1000px" w="90%" maxW="700px" m="0 auto">
+        <Box p="50px">
+          <Flex m="auto 0" flexDirection="column" justifyContent="center">
+            <Flex m="0 auto">
+              <Image src="/public/name_logo.png" w="500px" />
+            </Flex>
+
+            <Flex m="0 auto" w="80%" flexDirection="column" gap="30px">
+              <Flex flexDirection="column" gap="15px">
+                <Input placeholder="아이디(휴대폰 또는 이메일)" />
+                <InputGroup size="md">
+                  <Input pr="4.5rem" type={showPassword ? 'text' : 'password'} placeholder="비밀번호" />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleClick}>
+                      {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </Flex>
+
+              <Button colorScheme="brand">로그인</Button>
+              <Box position="relative" padding="10">
+                <Divider />
+                <AbsoluteCenter bg="white" px="4">
+                  또는
+                </AbsoluteCenter>
+              </Box>
+
+              <Button colorScheme="blue">구글 로그인</Button>
+              <Flex m="0 auto" w="60%" justifyContent="space-between">
+                <Link to="/detail">
+                  <Text>아이디 찾기</Text>
+                </Link>
+                <Link to="/detail">
+                  <Text>비밀번호 찾기</Text>
+                </Link>
+                <Link to="/detail">
+                  <Text> 회원가입</Text>
+                </Link>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
