9e331f952561d5a90055479262b7b486bca2d2f4
Ellsy23 <dlwnsgus3153@naver.com>
2023-11-28 18:38:50 +0900

    feat: footer

--- src/components/layouts/PageAside.tsx
+++ src/components/layouts/PageAside.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Github, Slack } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+import { Box, Image, Text, HStack, Divider } from '@chakra-ui/react';
 
 const PageAside: FC = () => {
   return (
@@ -11,18 +13,46 @@ const PageAside: FC = () => {
       top={0}
       paddingTop={{ base: 0, md: '8px' }}
       boxShadow="0 2px 2px -2px rgba(0,0,0,.3)"
-      backgroundColor="lightgray"
+      backgroundColor="gray.50"
       zIndex={999}
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      paddingX="4"
     >
-      <Flex align="center" justify="space-between" h="100%">
-        <Image src="/logo.png" alt="Logo" w="50px" h="50px" />
-        <Box borderLeft="1px solid #ccc" height="60%" mx="4" />
-        <Flex direction="column" justify="center" align="flex-end">
-          <Text fontSize="sm">개인정보처리방침</Text>
-          <Text fontSize="sm">이용약관</Text>
-          <Text fontSize="sm">이용안내</Text>
-        </Flex>
-      </Flex>
+      <HStack spacing="4" marginY="4">
+        <Image src="/public/logo.png" alt="Logo" w="100px" h="50px" />
+        <Text fontSize="sm" opacity={0.5}>
+          개인정보처리방침
+        </Text>
+        <Text fontSize="sm" opacity={0.5}>
+          이용약관
+        </Text>
+        <Text fontSize="sm" opacity={0.5}>
+          이용안내
+        </Text>
+      </HStack>
+
+      <Divider orientation="vertical" borderColor="gray.500" height="20px" marginY="4" width="2px" />
+
+      <HStack spacing="4">
+        <Link to="https://github.com/geezers-io/Tix2U">
+          <Github size="20px" />
+        </Link>
+        <Link to="https://app.slack.com/client/T05QT05KFE0/C05QHT2RUS2">
+          <Slack size="20px" />
+        </Link>
+      </HStack>
+
+      <Text fontWeight="bold" fontSize="sm" marginY="2">
+        OPERATING HOURS
+      </Text>
+      <Text fontSize="sm" opacity={0.5}>
+        MON - SUN PM 21:00 - AM 02:00
+      </Text>
+      <Text fontSize="sm" opacity={0.5}>
+        LUNCH AM 11:00 - PM 13:00
+      </Text>
     </Box>
   );
 };
