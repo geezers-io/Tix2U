2a28f152e9968a134f1a1047815fb954ee9d7294
Ellsy23 <dlwnsgus3153@naver.com>
2023-12-04 15:31:46 +0900

    chore: 절대경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import ConcertPage from './pages/concert/page';
-import DancingPage from './pages/dancing/page';
-import EntirePage from './pages/entire/page';
-import MusicalPage from './pages/musical/page';
-import TheaterPage from './pages/theater/page';
 import Layout from '@/components/layouts/PageLayout';
+import ConcertPage from '@/pages/concert/page';
+import DancingPage from '@/pages/dancing/page';
 import DetailPage from '@/pages/detail/[id]/page';
+import EntirePage from '@/pages/entire/page';
 import FindIDPage from '@/pages/login/find/page';
 import LoginPage from '@/pages/login/page';
+import MusicalPage from '@/pages/musical/page';
 import IndexPage from '@/pages/page';
+import TheaterPage from '@/pages/theater/page';
 import theme from '@/styles/theme';
 
 const router = createBrowserRouter([
