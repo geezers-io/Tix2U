ae5b2dd9331a6cff486c461165ad5fb87f3c5c40
heejung <acb4287@naver.com>
2023-12-10 22:33:47 +0900

    fix: 결제페이지 코드 수정

--- src/constants/detail.ts
+++ src/constants/detail.ts
@@ -1,4 +1,7 @@
 type saleList = { title: string; content: { subtitle: string; subcontent: string } }[];
+type payMethod = { title: string }[];
+type simplePayMethod = { image: string; name: string; value: string }[];
+
 export const ticketSale: saleList = [
   {
     title: '일반 할인',
@@ -13,5 +16,27 @@ export const ticketSale: saleList = [
       subtitle: '비씨카드',
       subcontent: '문화생활 할인 20%',
     },
+  },
+];
+
+export const payMethod: payMethod = [{ title: '카드결제' }, { title: '무통장 입금' }, { title: '휴대폰 결제' }];
+
+export const simplePayMethod: simplePayMethod = [
+  {
+    image: '/public/pay/tosspay.png',
+    name: '토스페이',
+    value: 'toss',
+  },
+
+  {
+    image: '/public/pay/kakaopay.png',
+    name: '카카오페이',
+    value: 'kakao',
+  },
+
+  {
+    image: '/public/pay/naverpay.png',
+    name: '네이버페이',
+    value: 'naver',
   },
 ];
