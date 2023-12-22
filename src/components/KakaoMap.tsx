c27fc821350cfc886c6dbab23bd5e8ba3d7968af
heejung <acb4287@naver.com>
2023-12-22 09:31:58 +0900

    Revert "fix: 카카오맵"
    This reverts commit 5289a601b4e8c84504807fcd9765849925e4bf09.

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import { FC } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { PerformanceDetail } from '@/api/services/PerformanceService.types';
@@ -7,23 +6,19 @@ interface Props {
   detail: PerformanceDetail;
 }
 const KakaoMap: FC<Props> = ({ detail }) => {
-  const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
   return (
-    <>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map
-        center={{
-          lat: 37.52112,
-          lng: 127.1283636,
-        }}
-        style={{ width: '100%', height: 'inherit' }}
-        level={3}
-      >
-        <MapMarker position={{ lat: 37.52112, lng: 127.1283636 }}>
-          <div style={{ color: '#000' }}>{detail.fcltynm}</div>
-        </MapMarker>
-      </Map>
-    </>
+    <Map
+      center={{
+        lat: 37.52112,
+        lng: 127.1283636,
+      }}
+      style={{ width: '100%', height: 'inherit' }}
+      level={3}
+    >
+      <MapMarker position={{ lat: 37.52112, lng: 127.1283636 }}>
+        <div style={{ color: '#000' }}>{detail.fcltynm}</div>
+      </MapMarker>
+    </Map>
   );
 };
 
